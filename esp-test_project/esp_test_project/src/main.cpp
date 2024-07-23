614b7db098dddc0463a950a2c1c9140bc15a3fc2
razi berg <razi.berg@gmail.com>
2024-07-23 21:31:42 +0300

    added bluetooth test code and it works

--- esp-test_project/esp_test_project/src/main.cpp
+++ esp-test_project/esp_test_project/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <string>
+#include <BluetoothSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial SerialBT;
+
+void bluetoothLoop();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+    Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    bluetoothLoop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void bluetoothLoop() {
+    if (Serial.available()) {
+        SerialBT.write(Serial.read());
+    }
+    if (SerialBT.available()) {
+        Serial.write(SerialBT.read());
+    }
+    delay(20);
 }
